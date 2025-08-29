4b39f2cd040f3924bd9a740163a8d9d7f5778eab
Franckie <franckie.dev@gmail.com>
2025-08-29 13:52:01 +0300

    feat(api): add fetchMarkets service to get coin market data

---
+++ src/services/fetchMarkets.ts
@@ -0,0 +1,24 @@
+import type { Coin, MarketsParams } from "@/types/types.ts";
+import { coinApi } from "./coinApi.ts";
+
+export const fetchMarkets = async (ids?: string[]): Promise<Coin[] | null> => {
+  const params: MarketsParams = {
+    vs_currency: "usd",
+    order: "market_cap_desc",
+    per_page: 20,
+    page: 1,
+    sparkline: false,
+  };
+
+  if (ids && ids.length > 0) {
+    params.ids = ids.join(",");
+  }
+
+  try {
+    const { data } = await coinApi.get("/coins/markets", { params });
+    return data;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
