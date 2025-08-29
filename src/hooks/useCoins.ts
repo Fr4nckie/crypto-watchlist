c77a73a57eb09930326c5bba4ba221f087dd2eea
Franckie <franckie.dev@gmail.com>
2025-08-29 14:00:07 +0300

    feat(hooks): create useCoins hook combining fetchSearch and fetchMarkets

---
+++ src/hooks/useCoins.ts
@@ -0,0 +1,34 @@
+import { fetchMarkets } from "@/services/fetchMarkets.ts";
+import { fetchSearch } from "@/services/fetchSearch.ts";
+import { useQuery } from "@tanstack/react-query";
+
+export const useCoins = (searchTerm: string) => {
+  const { data: searchResults, isFetching: isSearching } = useQuery({
+    queryKey: ["search", searchTerm],
+    queryFn: () => fetchSearch(searchTerm),
+    enabled: !!searchTerm,
+  });
+
+  const searchIds = searchResults?.map((coin) => coin.id);
+
+  const {
+    data: markets,
+    isLoading: isLoadingMarkets,
+    isError: isMarketsError,
+    error: marketsError,
+  } = useQuery({
+    queryKey: ["markets", searchTerm, searchIds],
+    queryFn: () =>
+      fetchMarkets(searchIds && searchIds.length > 0 ? searchIds : undefined),
+    enabled: Boolean(
+      !isSearching && (!searchTerm || (searchTerm && !!searchIds))
+    ),
+  });
+
+  return {
+    coins: markets ?? [],
+    isError: isMarketsError,
+    isLoading: isSearching || isLoadingMarkets,
+    error: marketsError,
+  };
+};
