96507400ba746b49a87fa9e78c6888e8f1f73593
Franckie <franckie.dev@gmail.com>
2025-08-29 13:47:03 +0300

    chore: remove unused files

--- src/hooks/useCoinsMarkets.ts
+++
@@ -1,9 +0,0 @@
-import { getCoinsMarkets } from "@/services/getCoinsMarkets.ts";
-import { useQuery } from "@tanstack/react-query";
-
-export const useCoinsMarkets = () => {
-  return useQuery({
-    queryKey: ["coins markets"],
-    queryFn: getCoinsMarkets,
-  });
-};
