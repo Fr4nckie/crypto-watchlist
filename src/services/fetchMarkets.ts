803bc4a00311a8505e9c961744a0a33e3ca8cbef
Franckie <franckie.dev@gmail.com>
2025-08-29 15:01:24 +0300

    refactor(fetchMarkets): increase per_page from 20 to 250

--- src/services/fetchMarkets.ts
+++ src/services/fetchMarkets.ts
@@ -5,7 +5,7 @@ export const fetchMarkets = async (ids?: string[]): Promise<Coin[] | null> => {
   const params: MarketsParams = {
     vs_currency: "usd",
     order: "market_cap_desc",
-    per_page: 20,
+    per_page: 250,
     page: 1,
     sparkline: false,
   };
@@ -16,6 +16,8 @@ export const fetchMarkets = async (ids?: string[]): Promise<Coin[] | null> => {
 
   try {
     const { data } = await coinApi.get("/coins/markets", { params });
+    console.log(data);
+
     return data;
   } catch (error) {
     console.log(error);
